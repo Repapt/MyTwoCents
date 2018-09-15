d30476b0be69f376f92c7b1d9288e1038a04867a
Sam <sam_hannat@outlook.com>
2018-09-15 08:57:08 -0400

    Added post request with time difference function

--- backend/chair/money/models.py
+++ backend/chair/money/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+import datedelta
+import datetime
 # Create your models here.
 class Survey(models.Model):
 	revenue = models.DecimalField(decimal_places=2, max_digits=8)
@@ -8,4 +9,16 @@ class Survey(models.Model):
 
 	frequency_of_pay = models.IntegerField()
 
+	def purchase_difference(self):
+		f = datedelta.datedelta(days=self.frequency_of_pay)
+		t = datetime.datetime.now().date()
+		s = self.start_of_pay
+		while (t - (s+f)).days > 0:
+			s+=f
 
+		days_left = s+f - t
+		if days_left.days == 7:
+			return 0
+		return days_left.days 
+
+	
--- backend/chair/money/views.py
+++ backend/chair/money/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from money.models import Survey
 from decimal import *
+import datetime
 # Create your views here.
 
 class moneyScore(APIView):
@@ -15,13 +16,27 @@ class moneyScore(APIView):
 			cost = Decimal(request.GET["cost"])
 		except Exception as e:
 			return Response("Please specify the cost in a numerical format")
+		print(s.purchase_difference())
 		return Response(algorithm(vals=s.__dict__))
 
 	def post(self, request):
+		args = ["revenue", "savings", "fpay"]
 		s = Survey()
+		data = []
 
+		
+		for arg in args:
+			data.append(request.data[arg])
+
+		s.revenue, s.savings, s.frequency_of_pay = data
+
+		s.start_of_pay = datetime.datetime.strptime(request.data["sdate"], "%Y-%m-%d").date()
+
+		s.save()
+		
+
+		print(request.data)
 		pass
-
 
 def algorithm(vals=[]):
 	print(vals)
